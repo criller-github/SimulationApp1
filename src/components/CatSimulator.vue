e318557ee833bddf375b84dba07d7482aadfec78
Julie <124886858+julieisager@users.noreply.github.com>
2024-11-04 09:03:06 +0100

    catSimulator tilført gif

--- src/components/CatSimulator.vue
+++ src/components/CatSimulator.vue
@@ -3,8 +3,9 @@
     <div class="header-container">
       <button id="helpButton" @click="useHelp">?</button>
       <h2>KATTESIMULATOR</h2>
-      <div><i class="fa-solid fa-money-bills fa-2x" style="color: #f0626e"></i>
-      <p>{{ money }}kr</p>
+      <div>
+        <i class="fa-solid fa-money-bills fa-2x" style="color: #f0626e"></i>
+        <p>{{ money }}kr</p>
       </div>
     </div>
     <div id="livesContainer">
@@ -15,18 +16,18 @@
 
     <div class="alert" v-if="alertMessage">
       <p>{{ alertMessage }}</p>
-      <button @click="dismissAlert"></button> <!-- Add a button to dismiss the alert -->
+      <button @click="dismissAlert"></button>
     </div>
 
     <div class="cat-container">
-        <iframe src="https://giphy.com/embed/H6Yv2GsSjBSnArRtfa" width="480" height="353" style="" frameBorder="0" class="giphy-embed cat-image" allowFullScreen></iframe>
+      <img :src="catGif" alt="Cat" class="cat-image">
     </div>
 
-     <div class="icon-container">
-         <button class="icon-box" @click="fodring"><i class="fa-solid fa-bowl-food fa-2x"></i></button>
-         <button class="icon-box" @click="Kattebakke"><i class="fa-solid fa-toilet fa-2x"></i></button>
-         <button class="icon-box" @click="Leg"><i class="fa-solid fa-heart fa-2x"></i></button>
-         <button class="icon-box" @click="Pleje"><i class="fa-solid fa-suitcase-medical fa-2x"></i></button>
+    <div class="icon-container">
+      <button class="icon-box" @click="fodring"><i class="fa-solid fa-bowl-food fa-2x"></i></button>
+      <button class="icon-box" @click="Kattebakke"><i class="fa-solid fa-toilet fa-2x"></i></button>
+      <button class="icon-box" @click="Leg"><i class="fa-solid fa-heart fa-2x"></i></button>
+      <button class="icon-box" @click="Pleje"><i class="fa-solid fa-suitcase-medical fa-2x"></i></button>
     </div>
   </div>
 </template>
@@ -40,12 +41,13 @@ export default {
     const lives = ref(Array(initialLives).fill(null));
     const message = ref('');
     const alertMessage = ref('');
-    const alertTimeout = ref(null); // New state for alert timeout
+    const alertTimeout = ref(null);
     const money = ref(300);
+    const catGif = ref(require('@/assets/cat.gif')); // Default gif
 
     const handleLifeLoss = () => {
       if (lives.value.length > 0) {
-        lives.value.pop(); // Remove a life
+        lives.value.pop();
         alertMessage.value = "Du har mistet et liv!";
       } else {
         alertMessage.value = "Ingen liv tilbage!";
@@ -53,53 +55,60 @@ export default {
     };
 
     const resetAlertTimeout = () => {
-      clearTimeout(alertTimeout.value); // Clear previous timeout
+      clearTimeout(alertTimeout.value);
       alertTimeout.value = setTimeout(() => {
-        handleLifeLoss(); // Lose a life if no response after 1 hour
-      }, 3600000); // 1 hour in milliseconds
+        handleLifeLoss();
+      }, 3600000);
     };
 
     const showAlert = (message) => {
       alertMessage.value = message;
-      resetAlertTimeout(); // Start the alert timeout when showing a new alert
+      resetAlertTimeout();
     };
 
     const dismissAlert = () => {
-      alertMessage.value = ''; // Clear the alert
-      clearTimeout(alertTimeout.value); // Clear the timeout when responding
+      alertMessage.value = '';
+      clearTimeout(alertTimeout.value);
     };
 
     const fodring = () => {
       if (money.value >= 20) {
         money.value -= 20;
         alertMessage.value = "Fodring udført! -20 kr";
+        catGif.value = require('@/assets/cat.gif'); // Skift gif til fodring
       } else {
         alertMessage.value = "Ikke nok penge til fodring!";
       }
-
       setTimeout(() => {
         alertMessage.value = "";
+        catGif.value = require('@/assets/cat.gif'); // Gå tilbage til standard gif
       }, 5000);
     };
 
     const Kattebakke = () => {
       alertMessage.value = "Rengøring af kattebakke udført!";
+      catGif.value = require('@/assets/cat.gif'); // Skift gif til kattebakke
       setTimeout(() => {
         alertMessage.value = "";
+        catGif.value = require('@/assets/cat.gif'); // Gå tilbage til standard gif
       }, 5000);
     };
 
     const Leg = () => {
       alertMessage.value = "Leg udført!";
+      catGif.value = require('@/assets/catLeg.gif'); // Skift gif til leg
       setTimeout(() => {
         alertMessage.value = "";
+        catGif.value = require('@/assets/cat.gif'); // Gå tilbage til standard gif
       }, 5000);
     };
 
     const Pleje = () => {
       alertMessage.value = "Pleje udført!";
+      catGif.value = require('@/assets/cat.gif'); // Skift gif til pleje
       setTimeout(() => {
         alertMessage.value = "";
+        catGif.value = require('@/assets/cat.gif'); // Gå tilbage til standard gif
       }, 5000);
     };
 
@@ -108,41 +117,46 @@ export default {
       setTimeout(() => {
         message.value = "";
       }, 5000);
-      handleLifeLoss(); // Lose a life when using help
+      handleLifeLoss();
     };
 
     const startRandomAlerts = () => {
-      // Fodring 3 gange dagligt
-      setInterval(() => {
+      const showFeedingAlert = () => {
         showAlert("Tid til fodring!");
-      }, getRandomInterval(3));
+        setTimeout(showFeedingAlert, getRandomInterval(3));
+      };
 
-      // Kattebakke 1-2 gange om ugen
-      setInterval(() => {
+      const showLitterBoxAlert = () => {
         showAlert("Tid til at tømme kattebakken!");
-      }, getRandomInterval(0.25)); // Ca. hver 2-4 dag
+        setTimeout(showLitterBoxAlert, getRandomInterval(0.25));
+      };
 
-      // Pleje 1 gang dagligt
-      setInterval(() => {
+      const showCareAlert = () => {
         showAlert("Tid til pleje!");
-      }, getRandomInterval(1));
+        setTimeout(showCareAlert, getRandomInterval(1));
+      };
 
-      // Leg 2 gange dagligt
-      setInterval(() => {
+      const showPlayAlert = () => {
         showAlert("Tid til leg!");
-      }, getRandomInterval(2));
+        setTimeout(showPlayAlert, getRandomInterval(2));
+      };
+
+      showFeedingAlert();
+      showLitterBoxAlert();
+      showCareAlert();
+      showPlayAlert();
     };
 
     const getRandomInterval = (timesPerDay) => {
-      const min = (24 / timesPerDay) * 60 * 60 * 1000 * 0.5; // Halvdelen af gennemsnitstiden
-      const max = (24 / timesPerDay) * 60 * 60 * 1000 * 1.5; // 1.5 gange gennemsnitstiden
+      if (timesPerDay <= 0) return 0;
+      const min = (24 / timesPerDay) * 60 * 60 * 1000 * 0.5;
+      const max = (24 / timesPerDay) * 60 * 60 * 1000 * 1.5;
       return Math.floor(Math.random() * (max - min) + min);
     };
 
     //const getRandomInterval = () => {
     //return 5000; // Sæt intervallet til 5 sekunder for hurtigere test
     //};
-
 
     onMounted(() => {
       startRandomAlerts();
@@ -159,11 +173,11 @@ export default {
       alertMessage,
       dismissAlert,
       money,
+      catGif, // Bind gif data til skærmen
     };
   }
 };
 </script>
-
 
 
 <style scoped>
@@ -225,21 +239,10 @@ export default {
 }
 
 .cat-image {
-  width: 100px;
+  width: 190px;
   height: auto;
 }
 
-.speech-bubble {
-  background-color: #f1f1f1;
-  border: 1px solid #ddd;
-  padding: 10px;
-  border-radius: 5px;
-  font-size: 14px;
-  font-weight: bold;
-  color: #333;
-  margin-top: 10px;
-  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
-}
 
 .icon-container {
   display: flex;
