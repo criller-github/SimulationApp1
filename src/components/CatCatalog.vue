873edf216ec1c6dcd91bea77b1777fb0ed44f69c
Julie <124886858+julieisager@users.noreply.github.com>
2024-12-15 04:06:56 +0100

    styling på CatCatalog.vue

--- src/components/CatCatalog.vue
+++ src/components/CatCatalog.vue
@@ -1,11 +1,18 @@
 <template>
-  <v-dialog :model-value="visible" @update:model-value="$emit('update:visible', $event)" persistent>
+  <v-dialog class="boks" :model-value="visible" @update:model-value="$emit('update:visible', $event)" persistent>
     <v-card>
-      <v-card-title>Katte Katalog</v-card-title>
+      <v-card-title class="d-flex justify-space-between">
+        Katte Katalog
+        <v-btn icon @click="$emit('update:visible', false)">
+          <svg-icon type="mdi" :path="path"></svg-icon>
+        </v-btn>
+      </v-card-title>
       <v-card-text>
         <div v-if="!selectedCategory">
           <!-- Vis kategorier -->
-          <v-btn @click="selectCategory('mad')">Mad</v-btn>
+          <v-btn @click="selectCategory('mad')">
+            <svg-icon type="mdi" :path="path2" class="mr-2"></svg-icon> Mad
+          </v-btn>
           <!--
           <v-btn @click="selectCategory('legetøj')">Legetøj</v-btn>
           <v-btn @click="selectCategory('rengøring')">Rengøring</v-btn>
@@ -14,31 +21,43 @@
         </div>
         <div v-else>
           <!-- Vis items i kategorien -->
-          <v-btn @click="selectedCategory = null">Tilbage</v-btn>
-          <v-list>
-            <v-list-item
+          <v-btn @click="selectedCategory = null" style="margin: 0 0 10px;">Tilbage</v-btn>
+          
+          <v-list >
+            <v-list-item class="card"
               v-for="item in shopItems"
               :key="item.id"
             >
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
+                <v-list-item-title class="font-weight-bold">{{ item.title }}</v-list-item-title>
                 <v-list-item-description>{{ item.description }}</v-list-item-description>
-                <div>Sultnedsættelse: {{ item.hungerGain }}</div>
-                <div>Vægtøgning: {{ item.weightGain }}</div>
-                <div>Anvendelser: {{ item.uses }}</div>
+
+                 <div>
+                    <svg-icon type="mdi" :path="path2" ></svg-icon>
+                    Sultnedsættelse: {{ item.hungerGain }}
+                </div>
+                <div>
+                    <svg-icon type="mdi" :path="path4"></svg-icon>
+                    Vægtøgning: {{ item.weightGain }}
+                </div>
+                <div>
+                    <svg-icon type="mdi" :path="path5"></svg-icon>
+                    Anvendelser: {{ item.uses }}
+                </div>
 
               {{ item.price }} kr <v-btn color="green" @click="buyItem(item)">Køb</v-btn>
             </v-list-item>
           </v-list>
+
         </div>
       </v-card-text>
-      <v-card-actions>
-        <v-btn @click="$emit('update:visible', false)">Luk</v-btn>
-      </v-card-actions>
+      
     </v-card>
   </v-dialog>
 </template>
 
 <script>
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiClose, mdiFood, mdiWeightGram , mdiRepeat } from '@mdi/js';
 import ShopItemDataService from '@/services/ShopItemDataService';
 
 export default {
@@ -46,10 +65,20 @@ export default {
   visible: Boolean,
   money: Number
     },
+
+  components: {
+    SvgIcon
+  },
+
   data() {
     return {
       selectedCategory: null,
-      shopItems: []
+      shopItems: [],
+      path: mdiClose,
+      path2: mdiFood,
+      path4: mdiWeightGram,
+      path5: mdiRepeat,
+      
     };
   },
   methods: {
@@ -69,3 +98,20 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.card {
+  margin: 0 30px 30px 0;
+  border: solid 1px #e0e0e0;
+  border-radius: 60px;
+  padding: 10px;
+  box-shadow: 0 1px 5px 1px #a3a2a2;
+}
+
+.boks {
+  position: absolute !important;
+  max-width: 450px;
+  height: 500px;
+}
+
+</style>
