0071d6a727e9ef15bb4832eeec4f0fe85032566d
“criller-github” <chri38nq@edu.zealand.dk>
2024-12-19 08:35:11 +0100

    h

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@
 import axios from "axios"; 
 
 export default axios.create({ //Opretter en ny Axios-instans 
-  baseURL: "http://localhost:8080/api", //Indstiller den grundlæggende URL for API'en: http://localhost:8080/api. Hver HTTP-anmodning (GET, POST, PUT osv.) sendt fra denne instans tilføjer automatisk denne base-URL
+  baseURL: "https://backendsimulatorapp.onrender.com", //Indstiller den grundlæggende URL for API'en: http://localhost:8080/api. Hver HTTP-anmodning (GET, POST, PUT osv.) sendt fra denne instans tilføjer automatisk denne base-URL
   headers: { //Angiver standardoverskrifter, som hver HTTP-anmodning skal inkludere
     "Content-type": "application/json", //Indstiller indholdstypen til JSON, da vi sender JSON-anmodninger til API'en
   },
